cb6e10f208fc9f8e45de996a981cfd069f2f2c65
Andrew Larson <andqwoo@gmail.com>
2019-11-15 01:43:08 -0600

    Add EqualDistributionCalculator

--- spec/models/equal_distribution_calculator_spec.rb
+++ spec/models/equal_distribution_calculator_spec.rb
@@ -2,6 +2,16 @@ require "spec_helper"
 
 RSpec.describe EqualDistributionCalculator do
   describe "calculate_average" do
-    it "returns the percentage of how equally capacity was distributed among trucks"
+    it "returns the percentage of how equally capacity was distributed among trucks" do
+      log = [
+        { capacity: 100, remaining_capacity: 10 },
+        { capacity: 100, remaining_capacity: 12 },
+        { capacity: 100, remaining_capacity: 5 },
+      ]
+
+      calculator = EqualDistributionCalculator.new(log: log)
+
+      expect(calculator.calculate_average).to eq(97.33)
+    end
   end
 end
